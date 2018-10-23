8eb23cf95a726aca98057c0d4a099b3ac368d55c
Sharon Kynaston <desmogal@gmail.com>
2018-10-23 11:33:52 +0100

    POJO Employee created

--- src/main/java/com/codeclan/employeeLab/EmployeeLab/Models/Employee.java
+++ src/main/java/com/codeclan/employeeLab/EmployeeLab/Models/Employee.java
@@ -1,21 +1,45 @@
 package com.codeclan.employeeLab.EmployeeLab.Models;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "First Name")
     private String first_name;
+    @Column(name = "Last Name")
     private String last_name;
+    @Column(name = "Employee Number")
     private int employeeNum;
-    private List<Project> projects;
+    @Column(name = "Department")
     private Department department;
+
+    @ManyToMany
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = {@JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+                    updatable = false)
+            }
+    )
+    private List<Project> projects;
 
     public Employee(String first_name , String last_name , int employeeNum , Department department) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.employeeNum = employeeNum;
-        this.projects = new ArrayList<> ( );
         this.department = department;
+        this.projects = new ArrayList<> ( );
+    }
+
+    public Employee() {
     }
 
     public String getFirst_name() {
